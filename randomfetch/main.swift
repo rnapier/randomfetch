5143028cdfea9e8a95786ad3c0d358582169ddfc
Rob Napier <robnapier@gmail.com>
2014-08-30 18:34:45 -0400

    Initial Commit

---
+++ randomfetch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  randomfetch
+//
+//  Created by Rob Napier on 8/30/14.
+//  Copyright (c) 2014 Rob Napier. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
